e587f5f4eec2314ac31e057333b99e8677e119f9
nitinsfdc <nvch66@gmail.com>
2020-10-17 00:03:13 +0530

    Added Login screen

--- src/bundles/login/login.js
+++ src/bundles/login/login.js
@@ -1,16 +1,25 @@
 import React from 'react';
-import {View ,Text, SafeAreaView, StyleSheet,Dimensions} from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import {View ,Text, SafeAreaView, StyleSheet,Dimensions,TouchableOpacity, ImageBackground, TextInput} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
+import Icon from 'react-native-vector-icons/FontAwesome';
+const bgImage = require('./../../res/images/cool-background.png');
 const {width,height} = Dimensions.get('window');
 export default class Login extends React.Component {
-  
-
+  constructor(props){
+    super(props);
+    this.state={
+        isVisible:true
+    }
+  }
   render() {
       return(
+          <ImageBackground
+          source={bgImage} style={styleSheet.image}
+          >
+          
         <SafeAreaView style={{flex:1}}>
                 <View style={styleSheet.contianer}>
-                    <View style={{flex:1}}>
+                    <View style={{flex:3}}>
                         <View style={styleSheet.titleStyle}>
                             <Text style={styleSheet.titleText}>
                                 Login
@@ -21,25 +30,92 @@ export default class Login extends React.Component {
                         </View>
                         <View style={styleSheet.socialLoginStyle}>
                             
-                            <LinearGradient colors={['skyblue', 'white']} 
+                        <TouchableOpacity
                             style={styleSheet.linearGradient}
                             >
-                            <Text style={styleSheet.buttonText}>
-                                F
-                            </Text>
-                            </LinearGradient>
-                            
-                            <LinearGradient colors={['skyblue', 'white']} 
+                           
+                                <Icon
+                                style={styleSheet.buttonText}
+                                name={'facebook-f'}
+                                />
+                           
+                           </TouchableOpacity>
+                            <TouchableOpacity
                             style={styleSheet.linearGradient}
                             >
-                            <Text style={styleSheet.buttonText}>
-                                G
-                            </Text>
-                            </LinearGradient>
+                           
+                                <Icon
+                                style={styleSheet.buttonText}
+                                name={'google'}
+                                />
+                           
+                           </TouchableOpacity>
                         </View>
+                        <View style={styleSheet.subTextContainer}>
+                            <Text style={styleSheet.subText}>
+                                or Login with Email
+                            </Text>
+                        </View>
+                    </View>
+                    <View style={{flex:4}}>
+                        <View style={styleSheet.loginFormContiner}>
+                            <View style={styleSheet.label}>
+                                <Text style={styleSheet.labelText}>
+                                    Email
+                                </Text>
+                            </View>
+                            <TextInput
+                            style={styleSheet.inputStyle}
+                            />
+
+                           
+                        </View>
+                        <View style={styleSheet.loginFormContiner}>
+                            <View style={styleSheet.label}>
+                                <Text style={styleSheet.labelText}>
+                                    Password
+                                </Text>
+                            </View>
+                            <TextInput
+                            style={styleSheet.inputStyle}
+                            secureTextEntry={this.state.isVisible}
+                            onFocus={()=>this.setState({
+                                color:'white'
+                            })}
+                            onEndEditing={(password)=>this.setState({
+                                password
+                            })}
+                            />
+                            
+                        </View>
+
+                    </View>
+                    <View style={{flex:1,bottom:60}}>
+                            
+                            <TouchableOpacity>
+                                <LinearGradient
+                                colors={['#32576b','#274352']}
+                                start={{ x: 0, y: 0 }} end={{ x: 1, y: 0 }}
+                                locations={[0.1,1]}
+                                style={styleSheet.signInBtn}
+                                >
+                                
+                                    <Text style={styleSheet.signInBtnText}>
+                                        Sign In
+                                    </Text>
+                                
+                                </LinearGradient>
+                                </TouchableOpacity>
+                           <View style={{alignItems:'center',marginTop:20}}>
+                               <Text style={styleSheet.subText}>
+                                   Don't have a account? <Text style={{color:'#32576b'}}>Register</Text>
+                               </Text>
+                           </View>
+
                     </View>
                 </View>
         </SafeAreaView>
+        </ImageBackground>
       )
   }
 }
@@ -61,13 +137,59 @@ const styleSheet = StyleSheet.create({
         flex: 1,
         paddingLeft: 15,
         paddingRight: 15,
-        borderRadius: 10
+        borderRadius: 10,
+        marginHorizontal:20,
+        marginTop:30,
+        backgroundColor:'#cad5df'
       },
       buttonText: {
-        fontSize: 18,
+        fontSize: 28,
         textAlign: 'center',
-        margin: 10,
-        color: '#ffffff',
+        margin: 16,
+        color: '#f5f5f5',
         backgroundColor: 'transparent',
       },
+      image: {
+        flex: 1,
+        resizeMode: "cover",
+        justifyContent: "center"
+      },
+      subTextContainer:{
+        marginTop:40,
+        alignItems:'center'
+      },
+      subText:{
+        color:'#9e9e9e'
+      },
+      loginFormContiner:{
+        marginHorizontal:20,
+      },
+      label:{
+          marginVertical:10
+      },
+      labelText:{fontSize:16},
+      inputStyle:{
+        backgroundColor:'#eceff1',
+        height:60,
+        borderRadius:10,
+        padding:10,
+        fontSize:20,
+        color:'#74a4c4'
+      },
+      signInBtn:{
+        paddingLeft: 15,
+        paddingRight: 15,
+        borderRadius: 15,
+        marginHorizontal:20,
+        marginTop:30,
+        backgroundColor:'#32576b'
+      },
+      signInBtnText:{
+        fontSize: 18,
+        textAlign: 'center',
+        padding:20,
+        color: 'white',
+        backgroundColor: 'transparent',
+        fontWeight:'500'
+      }
 })
